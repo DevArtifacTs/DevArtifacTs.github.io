bba6330713b5f358fc9c942e11e568376ce586b7
Jessada Srimoon <jessada_sr@hotmail.com>
2022-02-18 14:15:46 +0700

    Add intro to JS exercise

--- exercise-intro-to-javascript/james.js
+++ exercise-intro-to-javascript/james.js
@@ -11,3 +11,19 @@ function helloWorld() {
     alert(h1Message + ' แต่งไว้วิ่ง ไม่ใช่ซิ่งป่วนเมือง')
     
 }
+
+function calAge(){
+    const yearOfBirth = prompt("Enter your date of birth DEC", "");
+    const nowYear = new Date().getFullYear() ; 
+    const age =  nowYear - yearOfBirth ; 
+    alert(`You're ${age} years old`);
+}
+
+function loadProfile() {
+    const name = "Jessada";
+    const province = "Lamphun";
+    console.log(name);
+    const nameElement = document.querySelector("#my-name") ;
+    nameElement.innerHTML = name ;
+    const provinceElement = document.querySelector("#my-province").innerHTML = province ;
+}
